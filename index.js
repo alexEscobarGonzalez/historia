26e14d14c6efaa03b9a19778d66d4ff5b17a10c8
Alejandro Escobar Gonzalez <alexescobargonzalez5@gmail.com>
2022-05-31 07:04:52 +0200

    V1.1

---
+++ index.js
@@ -0,0 +1,28 @@
+var element1 = document.getElementById("card1")
+var button = element1.querySelector(".button-49")
+respuesta1 = element1.querySelector(".answer")
+button.addEventListener("click", e => {
+    respuesta1.classList.toggle("answer", )
+})
+
+var element2 = document.getElementById("card2")
+var button2 = element2.querySelector(".button-50")
+respuesta2 = element2.querySelector(".answer")
+button2.addEventListener("click", e => {
+    respuesta2.classList.toggle("answer", )
+})
+
+var element3 = document.getElementById("card3")
+var button3 = element3.querySelector(".button-51")
+respuesta3 = element3.querySelector(".answer")
+button3.addEventListener("click", e => {
+    respuesta3.classList.toggle("answer", )
+})
+
+var element4 = document.getElementById("card4")
+var button4 = element4.querySelector(".button-49")
+respuesta4 = element4.querySelector(".answer")
+button4.addEventListener("click", e => {
+    respuesta4.classList.toggle("answer")
+
+})
